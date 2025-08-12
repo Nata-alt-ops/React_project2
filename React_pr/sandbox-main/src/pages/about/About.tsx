8e90cebc28f1c25fb6a2b64715073f548b4e3bdf
n <nataliaklucnikova9@gmail.com>
2025-08-12 17:00:15 +0300

    \

--- React_pr/sandbox-main/src/pages/about/About.tsx
+++ React_pr/sandbox-main/src/pages/about/About.tsx
@@ -13,18 +13,53 @@ export const About = () => {
   const [error, setError] = useState(null);
 
    const [cityFilter, setCityFilter] = useState('all');
+   const [activeTab, setActiveTab] = useState<'history' | 'tours'>('history');
 
  
 
       return (
-        <div className='about_con1'>
+        <div className='p'>
+        <div className='about_con'>
             <div className='about_info_button'>
                 <div className='about_info'>
-
+                   {/* Первая колонка */}
+                  <div className='column'>
+                    <p className='t'>Население</p>
+                    <p className='h'>405 чел</p>
+                  </div>
+                  {/* Вторая колонка */}
+                  <div className='column'>
+                    <p className='t'>Казань-Иннополис</p>
+                    <p className='h'>1 ч.20 мин.</p>
+                  </div>
+                  {/* Третья колонка */}
+                  <div className='column'>
+                    <p className='t'>Казань-Иннополис</p>
+                    <p className='h'>42 км.</p>
+                  </div>
                 </div>
                 <div className='about_button'>
-                    
+                    <p onClick={() => navigate('/main')}>Вернуться</p>
                 </div>
             </div>
+            
+        </div>
+        <div className='information_con'>
+          <div className='info_con'>
+            <div className='photo_city'>
+                <div className='restangle'></div>
+                <img src='' alt=''></img>
+              </div>
+              {/*<div className='change_button'>
+                <button className={`tab-button ${activeTab === 'history' ? 'active' : ''}`}
+                  onClick={() => setActiveTab('history')}>История города</button>
+                  <button className={`tab-button ${activeTab === 'tours' ? 'active' : ''}`} onClick={() => setActiveTab('tours')}>Экскурсии по городу</button>
+                </div>*/}
+                
+               
+            </div>
+            </div>
+        
+           
         </div>
       )}
